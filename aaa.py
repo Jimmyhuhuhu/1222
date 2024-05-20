707ee6991d30d7d3770d32c45dcbc551d18d0b8f
Jimmyhuhuhu <162072558+Jimmyhuhuhu@users.noreply.github.com>
2024-05-20 13:24:57 +0800

    波型影片

--- aaa.py
+++ aaa.py
@@ -14,3 +14,19 @@ plt.ylabel('Y轴标签')
 
 # 显示图形
 plt.show()
+    # 步驟3：調整音訊的採樣率
+    target_sampling_rate = 16000
+    data, sampling_rate = librosa.load(io.BytesIO(audio_bytes), sr=target_sampling_rate)
+    
+    # 步驟4：進行傅立葉轉換
+    fft_data = np.fft.fft(audio_bytes)
+    
+    # 步驟5：顯示頻譜
+    frequencies = np.fft.fftfreq(len(fft_data),1 / target_sampling_rate) #1 / target_sampling_rate(需要分析的音檔長度，取樣間隔)
+    
+    plt.figure(figsize=(12, 4))
+    plt.plot(frequencies, np.abs(fft_data))
+    plt.title('Spectrum')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Magnitude')
+    plt.show()
